b944ffbdc1d9c9c6ef0dd9c2ba46cdc371244bf7
Jin-Linhao <jin_linhao@hotmail.com>
2016-06-27 11:33:39 +0800

    transfer bashrc file

--- src/depth_viewer.cpp
+++ src/depth_viewer.cpp
@@ -61,3 +61,32 @@ int main( int argc, char* argv[] )
     ros::Subscriber sub = n.subscribe("/camera/depth/image_rect_color", 3, &depthCb);
     ros::spin();
 }
+
+
+// source ~/git-completion.bash
+// # colors!
+// green="\[\033[0;32m\]"
+// blue="\[\033[0;34m\]"
+// purple="\[\033[0;35m\]"
+// reset="\[\033[0m\]"
+// # Change command prompt
+// source ~/git-prompt.sh
+// export GIT_PS1_SHOWDIRTYSTATE=1
+// # '\u' adds the name of the current user to the prompt
+// # '\$(__git_ps1)' adds git-related stuff
+// # '\W' adds the name of the current directory
+// export PS1="$purple\u$green\$(__git_ps1)$blue \W $ $reset"
+// alias ls="ls --color=auto"
+// alias ll="ls -al --color=auto"
+// alias ck="catkin_make"
+// alias gst="git status"
+// alias ros="source ~/myros.sh"
+// alias rosr="source ~/myrosr.sh"
+// alias ssh148="ssh ubuntu@192.168.1.148"
+// alias ssh133="ssh ubuntu@192.168.1.133"
+// alias freshtime="sudo ntpdate time time.nsit.gov"
+// export EDITOR='nano'
+
+// source /opt/ros/indigo/setup.bash
+// #source ~/myrosr.sh
+// source ~/catkin_ws/devel/setup.bash
